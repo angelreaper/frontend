065ce4727cd939d9353979ef5840817e8bf12f63
albertloz <alberloz20@gmail.com>
2024-05-16 19:19:39 -0500

    Se configura url de render para consumir backend

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 //para que sea usado por APIInvoke
+// module.exports = {
+//     api:{
+//         URL:'http://localhost:4000'
+//     }
+// }
+//url de para consumo desde render
 module.exports = {
     api:{
-        URL:'http://localhost:4000'
+        URL:'https://backend-w4ot.onrender.com'
     }
 }
