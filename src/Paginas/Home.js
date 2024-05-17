c2f6504973a9077c772a717ae53581fb9a9bba8c
albertloz <alberloz20@gmail.com>
2024-05-16 22:02:24 -0500

    Se corrigen algunos campos y se retiran menús adicionales

--- src/Paginas/Home.js
+++ src/Paginas/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
                             </div>
                         </div>
 
-                        <div className="row">
+                        {/* <div className="row">
 
                             <div className="col-lg-3 col-6">
                                 <div className="small-box bg-info">
@@ -51,9 +51,9 @@ const Home = () => {
                                     <Link to={"/proveedores"} className="small-box-footer">Ver Proveedores <i className="fas fa-arrow-circle-right" /></Link>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
 
-                        <div className="row">
+                        {/* <div className="row">
 
                             <div className="col-lg-3 col-6">
                                 <div className="small-box bg-info">
@@ -67,7 +67,7 @@ const Home = () => {
                                     <Link to={"/Productos"} className="small-box-footer">Ver Productos <i className="fas fa-arrow-circle-right" /></Link>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </section>
 
